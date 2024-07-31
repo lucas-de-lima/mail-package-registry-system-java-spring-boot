6b3a3f3954d29ddbcea64160a2f92ff0187f8217
lucas-de-lima <lucasramosdelima@gmail.com>
2024-07-31 14:27:46 -0300

    feat: up main project

---
+++ src/main/java/com/lucasdelima/MailPackageRegistrySystemApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdelima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailPackageRegistrySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailPackageRegistrySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasdelima/MailPackageRegistrySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdelima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailPackageRegistrySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
