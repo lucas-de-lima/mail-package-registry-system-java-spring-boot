9efe6458f2d40cfc80ab7c74f090dac1970e49fb
lucas-de-lima <lucasramosdelima@gmail.com>
2024-08-02 14:20:39 -0300

    feat: create RecipientImpl class

---
+++ src/main/java/com/lucasdelima/database/RecipientImpl.java
@@ -0,0 +1,33 @@
+package com.lucasdelima.database;
+
+import com.lucasdelima.dao.RecipientDAO;
+import com.lucasdelima.dto.RecipientDTO;
+
+import java.util.List;
+
+public class RecipientImpl implements RecipientDAO {
+    @Override
+    public void save(RecipientDTO entity) {
+
+    }
+
+    @Override
+    public void update(RecipientDTO entity) {
+
+    }
+
+    @Override
+    public void delete(RecipientDTO entity) {
+
+    }
+
+    @Override
+    public RecipientDTO findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<RecipientDTO> findAll() {
+        return List.of();
+    }
+}
